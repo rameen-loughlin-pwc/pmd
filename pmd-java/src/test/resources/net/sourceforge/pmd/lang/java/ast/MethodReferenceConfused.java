37c4f1ba1c613e97ef44aa50b0ac0776f99b4e0f
rameen-loughlin-pwc <77490837+rameen-loughlin-pwc@users.noreply.github.com>
2021-01-28 12:13:18 +0000

    Delete pmd-java directory

--- pmd-java/src/test/resources/net/sourceforge/pmd/lang/java/ast/MethodReferenceConfused.java
+++
@@ -1,8 +0,0 @@
-public class MethodReferenceConfused {
-    public void wrongVariableAccessor() {
-        Object someVarNameSameAsMethodReference = null;
-        Object someObject = null;
-        String result = AccessController.doPrivileged((PrivilegedAction<String>)
-                ((SomeCast) someObject)::someVarNameSameAsMethodReference);
-    }
-}
